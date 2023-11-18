4350448f6b818c2fc409408e0b8f357dda03c041
Ardal-ON <ardalanaryashad@gmail.com>
2023-11-17 19:32:25 -0800

    HomePage visual Elements Added

---
+++ myapp/Home.py
@@ -0,0 +1,134 @@
+import flet as ft
+
+class HomePage(ft.UserControl):
+    def __int__(self):
+        self._Homepage = None
+        self._greeting = None
+        self._card = None
+        self._parameterlist = None
+        super().__int__()
+
+    def Greeting(self):
+        self._greeting =  ft.Container(
+            content=ft.Text(
+                        "Hello, Ardalan!",
+                        size=25,
+                        font_family="RobotoSlab",
+                        weight=ft.FontWeight.W_800,
+                        color=ft.colors.RED_600,
+            ),
+            alignment=ft.alignment.center,
+            margin=ft.margin.only(top=10),
+            padding=-10,
+        )
+    
+    def Card(self):
+        self._card = ft.Container(
+            ft.Column([
+                ft.Container(
+                    ft.Text(
+                        "Ford Focus 2018",
+                        color=ft.colors.BLACK,
+                        size=24,
+                        weight="W800",
+                    ),
+                    #margin=ft.margin.only(left=10,top=10)
+                ),
+                ft.Container(
+                    ft.Text(
+                        "Ardalan's Car",
+                        color=ft.colors.BLACK,
+                        size=14,
+                        weight="w600",
+                    ),
+                    #margin=ft.margin.only(left=10)
+                ),
+                ft.Container(
+                    ft.Text(
+                        "CA#7RSV070",
+                        color=ft.colors.BLACK,
+                        size=13,
+                        weight="w500",
+                    ),
+                    #margin=ft.margin.only(left=10)
+                ),
+                ft.Container(
+                    ft.Image(src='Car.png',fit=ft.ImageFit.FIT_HEIGHT),
+                    width=360,
+                    height=80,
+                    alignment= ft.alignment.Alignment(0.95,0.75),
+                    margin=ft.margin.only(top=-15)
+                ),
+            ],spacing=0),
+            width=360,
+            height=160,
+            #margin=ft.margin.only(top=20),
+            padding=10,
+            bgcolor=ft.colors.GREEN_300,
+            border_radius=12,
+        )
+    
+    def ParameterList(self):
+
+
+        self._parameterlist = ft.Column(
+            controls=[
+                ft.Container(
+                    content= ft.Text(
+                                "Engine Healt Parameters",
+                                text_align=ft.TextAlign.CENTER,
+                                weight="w600",
+                                size=20,),
+                    alignment=ft.alignment.center,
+                    padding=-5,
+                ),
+                ft.TextField(
+                    label="Engine RPM",
+                    suffix_text="RPM",
+                    keyboard_type=ft.KeyboardType.NUMBER,
+                    prefix_icon=ft.icons.SPEED),
+                ft.TextField(
+                    label="Lub oil pressure",
+                    suffix_text="Pa",
+                    keyboard_type=ft.KeyboardType.NUMBER,
+                    prefix_icon=ft.icons.OIL_BARREL),
+                ft.TextField(
+                    label="Fuel pressure",
+                    suffix_text="Pa",
+                    keyboard_type=ft.KeyboardType.NUMBER,
+                    prefix_icon=ft.icons.DISC_FULL),
+                ft.TextField(
+                    label="Coolant pressure",
+                    suffix_text="Pa",
+                    keyboard_type=ft.KeyboardType.NUMBER,
+                    prefix_icon=ft.icons.WATER_DROP),
+                ft.TextField(
+                    label="Lub oil temperature",
+                    suffix_text="°C",
+                    keyboard_type=ft.KeyboardType.NUMBER,
+                    prefix_icon=ft.icons.HOT_TUB),
+                ft.TextField(
+                    label="Coolant temperature",
+                    suffix_text="°C",
+                    keyboard_type=ft.KeyboardType.NUMBER,
+                    prefix_icon=ft.icons.THERMOSTAT)
+            ],
+            alignment=ft.alignment.center,
+        )
+
+    def build(self):
+        self.Greeting()
+        self.Card()
+        self.ParameterList()
+        
+        self._Homepage = ft.Container(
+                        width=360,
+                        height=740,
+                        content=ft.Column(
+                            [self._greeting,
+                             self._card,
+                             self._parameterlist,]
+                        )
+                        )
+
+        return self._Homepage
--- myapp/main.py
+++ myapp/main.py
@@ -3,96 +3,7 @@ import datetime
 import calendar
 from calendar import HTMLCalendar
 from dateutil import relativedelta
-
-class AnimatedCard(ft.UserControl):
-    def __int__(self):
-        super().__int__()
-
-    def build(self):
-        self._icon_container_ = ft.Container(
-            width=120,
-            height=35,
-            bgcolor=ft.colors.BLUE_800,
-            border_radius=25,
-            animate_opacity=200,
-            offset=ft.transform.Offset(0, 0.25),
-            animate_offset=ft.animation.Animation(duration=900, curve="ease"),
-            visible=False,
-            content=ft.Row(
-                alignment="center",
-                vertical_alignment="center",
-                controls=[
-                    ft.Text(
-                        "More Info",
-                        size=12,
-                        weight="w600",
-                    ),
-                ],
-            ),
-        )
-
-        self._container = ft.Container(
-            #width=280,
-            #height=380,
-            bgcolor=ft.colors.WHITE,
-            border_radius=12,
-            #on_hover=lambda e: self.AnimatedCardHover(e),
-            animate=ft.animation.Animation(600, "ease"),
-            border=ft.border.all(2, ft.colors.WHITE24),
-            content=ft.Column(
-                alignment="center",
-                horizontal_alignment="start",
-                spacing=0,
-                controls=[
-                    ft.Container(
-                        padding=20,
-                        alignment=ft.alignment.bottom_center,
-                        content=ft.Text(
-                            "Card Title",
-                            color=ft.colors.BLACK,
-                            size=28,
-                            weight="w800",
-                        ),
-                    ),
-                    ft.Container(
-                        padding=20,
-                        alignment=ft.alignment.top_center,
-                        content=ft.Text(
-                            "Insert card details here...",
-                            color=ft.colors.BLACK,
-                            size=14,
-                            weight="w500",
-                        ),
-                    ),
-                ],
-            ),
-        )
-
-        self.__card = ft.Card(
-            elevation=0,
-            content=ft.Container(
-                content=ft.Column(
-                    spacing=0,
-                    horizontal_alignment="center",
-                    controls=[
-                        self._container,
-                    ],
-                ),
-            ),
-        )
-
-        self._card = ft.Column(
-            horizontal_alignment="start",
-            spacing=0,
-            controls=[
-                self.__card,
-                self._icon_container_,
-            ],
-        )
-
-        self._main = self._card
-
-        return self._main
+from Home import HomePage
 
 class FletCalendar(ft.UserControl):
     
@@ -388,9 +299,16 @@ class MainPage(ft.UserControl):
 
 
 def main(page: ft.Page):
+    page.window_width = 380
+    page.window_height = 760
+    page.window_min_height = 760
+    page.window_min_width = 380
+    page.scroll = ft.ScrollMode.HIDDEN
     page.vertical_alignment = ft.MainAxisAlignment.START
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    
+    page.padding = 0
+    page.title = "My Application"
+
     def changetab(navbar):
 	# GET INDEX TAB
         my_index = navbar.control.selected_index
@@ -415,14 +333,25 @@ def main(page: ft.Page):
                             ]
         )
 
-    home = AnimatedCard()
+    home = HomePage()
+
     dashboard = MainStackContainer()
     dashboard.visible = False
+    
     maintenance = FletCalendar()
     maintenance.visible = False
-    page.add(home,dashboard,maintenance)
+    
+    body = ft.Container(
+        ft.Stack([
+            home,
+            dashboard,
+            maintenance,
+        ]),
+    )
+    
+    page.add(body)
     page.update()
     
 
-#ft.app(target=main)
-ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+ft.app(target=main, assets_dir="pic")
+#ft.app(target=main, view=ft.AppView.WEB_BROWSER, assets_dir="pic")
